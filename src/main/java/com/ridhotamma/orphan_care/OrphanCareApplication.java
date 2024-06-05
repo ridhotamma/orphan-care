9849396ca6f6aae859a2ac195b7922e2ca4e627d
Ridho Tamma <ridho.tamma@gmail.com>
2024-06-05 18:53:44 +0700

    initial codebase

---
+++ src/main/java/com/ridhotamma/orphan_care/OrphanCareApplication.java
@@ -0,0 +1,13 @@
+package com.ridhotamma.orphan_care;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrphanCareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrphanCareApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ridhotamma/orphan_care/OrphanCareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ridhotamma.orphan_care;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrphanCareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
