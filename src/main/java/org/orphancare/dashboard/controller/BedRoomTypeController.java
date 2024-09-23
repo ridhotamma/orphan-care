cbc149a3f18521bf1e9c1637d3daa15949b36298
Ridho Tamma <ridho.tamma@gmail.com>
2024-09-23 23:48:45 +0700

    add preauthorize to bedroom type controller

--- src/main/java/org/orphancare/dashboard/controller/BedRoomTypeController.java
+++ src/main/java/org/orphancare/dashboard/controller/BedRoomTypeController.java
@@ -5,6 +5,7 @@ import lombok.RequiredArgsConstructor;
 import org.orphancare.dashboard.dto.BedRoomTypeDto;
 import org.orphancare.dashboard.service.BedRoomTypeService;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -18,30 +19,35 @@ public class BedRoomTypeController {
     private final BedRoomTypeService bedRoomTypeService;
 
     @GetMapping
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
     public ResponseEntity<List<BedRoomTypeDto>> getBedRoomTypes() {
         List<BedRoomTypeDto> bedRoomTypes = bedRoomTypeService.getAllBedRoomTypes();
         return ResponseEntity.ok(bedRoomTypes);
     }
 
     @GetMapping("/{id}")
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
     public ResponseEntity<BedRoomTypeDto> getBedRoomTypeById(@PathVariable UUID id) {
         BedRoomTypeDto bedRoom = bedRoomTypeService.getBedRoomTypeById(id);
         return ResponseEntity.ok(bedRoom);
     }
 
     @PostMapping
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
     public ResponseEntity<BedRoomTypeDto> createBedRoomType(@RequestBody @Valid BedRoomTypeDto bedRoomTypeDto) {
         BedRoomTypeDto createdBedRoomType = bedRoomTypeService.createBedRoomType(bedRoomTypeDto);
         return ResponseEntity.status(201).body(createdBedRoomType);
     }
 
     @PutMapping("/{id}")
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
     public ResponseEntity<BedRoomTypeDto> updateBedRoomType(@PathVariable UUID id, @RequestBody @Valid BedRoomTypeDto bedRoomTypeDto) {
         BedRoomTypeDto updatedBedRoomType = bedRoomTypeService.updateBedRoomType(id, bedRoomTypeDto);
         return ResponseEntity.ok(updatedBedRoomType);
     }
 
     @DeleteMapping("/{id}")
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
     public ResponseEntity<Void> deleteBedRoomType(@PathVariable UUID id) {
         bedRoomTypeService.deleteBedRoomTypeById(id);
         return ResponseEntity.noContent().build();
