6b2eee97d4a8f11579c6254a2011f15a2babcc13
Ridho Tamma <ridho.tamma@gmail.com>
2024-10-28 10:39:25 +0700

    update address mapper

--- src/main/java/org/orphancare/dashboard/mapper/AddressMapper.java
+++ src/main/java/org/orphancare/dashboard/mapper/AddressMapper.java
@@ -12,6 +12,10 @@ public interface AddressMapper {
     @Mapping(target = "regencyId", source = "regencyDetail.id")
     @Mapping(target = "districtId", source = "districtDetail.id")
     @Mapping(target = "villageId", source = "villageDetail.id")
+    @Mapping(target = "province", expression = "java(getProvinceName(dto))")
+    @Mapping(target = "regency", expression = "java(getRegencyName(dto))")
+    @Mapping(target = "district", expression = "java(getDistrictName(dto))")
+    @Mapping(target = "village", expression = "java(getVillageName(dto))")
     Address toEntity(AddressDto dto);
 
     @Mapping(target = "provinceDetail", expression = "java(mapProvinceDetail(entity))")
@@ -19,6 +23,34 @@ public interface AddressMapper {
     @Mapping(target = "districtDetail", expression = "java(mapDistrictDetail(entity))")
     @Mapping(target = "villageDetail", expression = "java(mapVillageDetail(entity))")
     AddressDto toDto(Address entity);
+
+    default String getProvinceName(AddressDto dto) {
+        if (dto.getProvinceDetail() != null && dto.getProvinceDetail().getName() != null) {
+            return dto.getProvinceDetail().getName();
+        }
+        return dto.getProvince();
+    }
+
+    default String getRegencyName(AddressDto dto) {
+        if (dto.getRegencyDetail() != null && dto.getRegencyDetail().getName() != null) {
+            return dto.getRegencyDetail().getName();
+        }
+        return dto.getRegency();
+    }
+
+    default String getDistrictName(AddressDto dto) {
+        if (dto.getDistrictDetail() != null && dto.getDistrictDetail().getName() != null) {
+            return dto.getDistrictDetail().getName();
+        }
+        return dto.getDistrict();
+    }
+
+    default String getVillageName(AddressDto dto) {
+        if (dto.getVillageDetail() != null && dto.getVillageDetail().getName() != null) {
+            return dto.getVillageDetail().getName();
+        }
+        return dto.getVillage();
+    }
 
     default AddressDto.ProvinceDto mapProvinceDetail(Address entity) {
         if (entity.getProvinceId() == null) return null;
